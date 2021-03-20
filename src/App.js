033a163627c5ef2e9264f1e992feeea90a11daf5
MuhammadJawad <chudhryjawad@gmail.com>
2021-03-20 10:36:13 +0500

    Amazon-Clone with advance hooks usage.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Banner from './Banner';
+import Basket from './Basket';
+import BasketValues from './BasketValues';
+import LogInPage from './LogInPage';
+import NavBar from './NavBar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Switch>
+        <Route exact path='/'  ><NavBar/><Banner/></Route>
+        <Route exact path='/login' component={LogInPage} />
+        <Route exact path='/basket' component={Basket} ><NavBar/> <BasketValues/> </Route>
+        <Redirect to='/' />
+        {/* <Redirect to='' */}
+      </Switch>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/InterLink.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InterLink() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default InterLink
